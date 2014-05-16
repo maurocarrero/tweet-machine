9fe0b2ec3267fa7d319d8d70836275a6f1ddd352
Matias Figueroa <mfigueroa@salesforce.com>
2014-05-16 16:49:01 -0300

    La que se invento mati...

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,25 +2,34 @@
 
 angular.module('tweetMachineApp')
   .controller('MainCtrl', function ($scope, $window) {
-    
-  	var tweets = $window.document.getElementsByClassName('tweet'),
-  		tweetsLength = tweets.length,
-  		opacity = 100 / tweetsLength;
 
-  	angular.forEach(tweets, function (tweet, $index) {
-  		var styles = $window.getComputedStyle(tweet),
-  			idx = $index + 1,
-  			margin = idx * 40,
-  			thisOpacity = idx * opacity;
 
-  		tweet.style.zIndex = idx;
-  		tweet.style.marginTop += margin + 'px';
-  		tweet.style.opacity = idx === tweetsLength ? '1' : '0.' + thisOpacity;
-  		tweet.style.webkitTransform = 'scale(1.' + idx + ')';
-  	});
+    function reflow() {
+    	var tweets = $window.document.getElementsByClassName('tweet'),
+    		tweetsLength = tweets.length,
+    		opacity = 100 / tweetsLength;
+
+    	angular.forEach(tweets, function (tweet, $index) {
+    		var styles = $window.getComputedStyle(tweet),
+    			idx = $index + 1,
+    			margin = idx * 40,
+    			thisOpacity = idx * opacity;
+
+    		// tweet.style.zIndex = idx;
+    		//tweet.style.marginTop += margin + 'px';
+    		tweet.style.opacity = idx === tweetsLength ? '1' : '0.' + thisOpacity;
+    		tweet.style.webkitTransform = 'scale(1.' + idx + ')';
+    		tweet.style.webkitTransform += ' translateY('+ idx*30 +'px)';
+    	});
+    }
+
+    reflow();
 
   	$scope.nextTweet = function () {
-  		console.log('next');  		
+  		var tweets = $window.document.getElementsByClassName('tweet'),
+          tweetsContainer = tweets[0].parentNode;
+      tweetsContainer.insertBefore(tweetsContainer.firstChild, tweetsContainer.lastChild);
+      reflow();
   	};
 
   	$scope.prevTweet = function () {
